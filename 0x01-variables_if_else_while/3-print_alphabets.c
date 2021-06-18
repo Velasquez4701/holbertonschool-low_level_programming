56739568f97c40ce2fbc754c9b614279c32d32b9
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-18 19:23:33 +0000

    alphabet uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - alphabet uppercase
  *
  * Return: 0 Always
@@ -9,14 +9,14 @@ int main(void)
 {
 	char ch;
 
-    	for (ch = 'a'; ch <= 'z'; ch++)
-    	{
-        	putchar(ch);
-    	}
-    	for (ch = 'A'; ch <= 'Z'; ch++)
-    	{
-        	putchar(ch);
-    	}
-    	putchar('\n');
-    	return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n);
+	return (0);
 }
