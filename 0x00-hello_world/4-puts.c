f2504f80d591e3ff1e4181f621a4220a18cf58d5
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-18 00:28:38 +0000

    programming in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <strin.h>
+
+/**
+ * main-prints the phrase "Programming is like building a multilingual puzzle
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
