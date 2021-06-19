fe451cecd051a8e69638a7e783b37387421a69bf
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-19 01:01:05 +0000

    combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - combinations of single digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
