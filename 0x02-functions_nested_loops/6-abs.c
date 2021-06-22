0777ff668756b99c0ac0b091ad31dbf9d4c97d0e
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-22 22:38:32 +0000

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,9 +3,14 @@
 /**
  * _abs - absolute value
  * @n: value
- * Return: n
+ * Return: absolute value
  */
 int _abs(int n)
 {
-	int x = abs(n);
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+
+	return (0);
 }
