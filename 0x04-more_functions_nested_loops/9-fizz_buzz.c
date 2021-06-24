9b2c44dd9c035b8b0edb23e50baa5d97e5cc60d4
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-24 21:39:55 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 
 /**
   * main - Prints a Fizz Buzz program
@@ -13,18 +13,29 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
 		else if (n % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (n % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%d", n);
+		}
 
-		if (x != 100)
+		if (n != 100)
+		{
 			printf(" ");
-
+		}
 	}
+
 	printf("\n");
+
 	return (0);
 }
