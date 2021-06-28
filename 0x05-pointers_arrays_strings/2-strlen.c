d11bfe59029d5f6b158214fb8d156c0a06adb704
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-28 16:35:28 +0000

    Returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - length of a string
+ * @s: parameter
+ * Return: s
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+}
