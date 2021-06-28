85062c621dd53c0b20feed69f46b61dbd4cb089f
Luis Velasquez <luisvelasques4701@gmail.com>
2021-06-28 22:28:24 +0000

    Prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,14 @@ void print_rev(char *s)
 	int index = 0;
 
 	while (s[index] != '\0')
+	{
 		index++;
+	}
 
-	for (idnex = index - 1; index >= 0; index--)
-		_putchar(s[idnex]);
+	for (index = index - 1; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
 
 	_putchar('\n');
 }
