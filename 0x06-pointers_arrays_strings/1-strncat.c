f1668a4292e327d30e311c61c59d9d4b0a99b881
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-02 03:07:35 +0000

    concatenates two string with bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int index = strlen(dest);
+	int a = 0;
 
+	while (a < n && *src)
+	{
+		dest[index + a] = *src;
+		src++;
+		a++;
+	}
+	dest[index + a] = '\0';
 	return (dest);
 }
