412d397bc2cb5c6d1314e3847daef6ac5b5c11c6
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-02 05:06:25 +0000

    reverse the content of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Print array in reverse
+ * @a: array
+ * @n: size of array
+ *
+ * Return: Array in reverse
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	j = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
+	}
+}
