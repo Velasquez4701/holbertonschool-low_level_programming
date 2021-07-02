bd3c437929c34b30d019257b2a76050b2bba78b1
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-02 04:39:22 +0000

    copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Copy string
+ * @dest: First string
+ * @src: Second string
+ * @n: bytes
+ *
+ * Return: Concatenation
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *src)
+	{
+		dest[i] = *src;
+		src++;
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
