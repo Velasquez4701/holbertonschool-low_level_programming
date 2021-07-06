bc5cc50cc3295bff8eb01423890fcb2336c1e3da
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-06 00:10:07 +0000

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * _memcpy - Copies memory with a constant byte
- * @dest: a pointer to the memory
+ * _memcpy - copies bytes from the memory
+ * @dest: pointer to the memory
  * @src: source buffer to copy
- * @n: bytes of memory
+ * @n: number of bytes
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int  i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
