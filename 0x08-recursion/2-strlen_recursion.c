d55e609489174024861106667f4c13c384171ed0
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-07 21:42:53 +0000

    length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,8 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	int i = 0;
+
+	if (*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
+	return (i);
 }
