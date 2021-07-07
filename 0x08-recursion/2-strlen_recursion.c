4e4ac1fc5ff51925cdfb2348363c76ee5410f1ff
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-07 21:23:33 +0000

    length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - strlen recursion
+ * @s: string
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
