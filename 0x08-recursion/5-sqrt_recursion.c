9b0f9382844cf59d9727b10535748c2029bbf709
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-08 04:12:20 +0000

    natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * find_square - find square
+ * @x: number
+ * @y: sqr of x
+ * Return: sqr options
+ */
+int find_square(int x, int y)
+{
+	int square = y * y;
+
+	if (square > x)
+		return (-1);
+
+	if (square == x)
+		return (y);
+	else
+		return (find_square(x, y + 1));
+}
+
+/**
+ * _sqrt_recursion - function of call
+ * @n: number
+ * Return: find square
+ */
+int _sqrt_recursion(int n);
+{
+	return (find_square(n, 1));
+}
