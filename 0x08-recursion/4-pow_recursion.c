c74887f3a4b29586de298a3729e2533aa6f04834
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-08 00:52:17 +0000

    value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - power at two numbers
+ * @x: value
+ * @y: value
+ * Return: pow
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
