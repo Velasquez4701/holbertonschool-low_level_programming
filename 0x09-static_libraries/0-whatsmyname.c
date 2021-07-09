a3e21ffb9867aee5365e52b17c19d8226d5f9ea0
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-09 19:44:46 +0000

    prints its name

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument
+ * @arfv: pointer
+ * Return: argv[]
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0];
+	return (0);
+}
