a5fe6f98d509e5f57ea0f88284f468aef3699570
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-09 23:26:16 +0000

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print all argument it receive
+ * @argc: argument
+ * @argv: pointer
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
