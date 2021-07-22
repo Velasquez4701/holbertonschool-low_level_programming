9fc16d2862d20378431858b1cc4a433597d7d361
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-22 04:59:22 +0000

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - perform simple operations
+ * @argc: array count
+ * @argv: array of pointers to arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+}
