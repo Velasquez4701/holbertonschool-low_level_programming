318be26e3b4ac591e7807ac2cfd745169d32ab92
Luis Velasquez <luisvelasques4701@gmail.com>
2021-07-22 02:26:01 +0000

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - print a name
+ * @name: name of the person
+ * @f: Pointer to function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
